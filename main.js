cba32834b1d6a95acb69c2a80152e2bccc123067
embeddedLinuxGuy <embeddedLinuxGuy@gmail.com>
2017-12-28 16:03:33 -0800

    close db connection after queries finish

--- main.js
+++ main.js
@@ -36,26 +36,42 @@ var red = "\033[31m";
 var green = "\033[32m";
 var yellow = "\033[33m";
 
+var extend_chain = function(from_visit) {
+    connection.query(get_parent_query, [ from_visit ], function (e, r, f) {
+    });
+};
+
+// place_id: place visited; index to moz_places
+// from_visit: referrer; index to moz_historyvisits
+
+// return: length of referrer chain. 0 for no referrers.
 var chain_length = function(place_id, from_visit) {
-    console.log(`Chain length for ${place_id} is: ???`);
+//    console.log(`Chain length for ${place_id} is: ???`);
     connection.query(get_parent_query, [ from_visit ], function (e, r, f) {
 	if (e) throw e;
 	//	console.log(r[0].from_visit);
 	//	console.log(r.length);
 	if (r.length === 1) {
 //	    console.log("Good entry");
+//	    extend_chain();
 	} else if (r.length === 0) {
-//	    console.log("Empty entry");
+	    //	    console.log("Empty entry");
+//	    return 0;
 	} else {
 	    console.log(r.length);
+	}
+	if (--con_count === 0) {
+	    connection.end();
 	}
     });
 }
 
+var con_count = 0;
 connection.query(get_all_visits_query, function (e, r, f) {
     if (e) throw e;
+    con_count = r.length;
     for (i=0; i < r.length; ++i) {
-	console.log(`${r[i].place_id}, ${r[i].from_visit}`);
+//	console.log(`${r[i].place_id}, ${r[i].from_visit}`);
 	chain_length(r[i].place_id, r[i].from_visit);
     }
 });
